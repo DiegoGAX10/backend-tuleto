93a787ccce99deb6466b698c0ae3e7a899392668
DiegoGAX10 <dieegoortiz24@gmail.com>
2026-03-06 13:53:15 -0600

    inicio

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from sshtunnel import SSHTunnelForwarder
 import mysql.connector
 import warnings
 import traceback
+import os
 
 warnings.filterwarnings("ignore")
 
@@ -121,4 +122,5 @@ def get_stats():
             tunnel.stop()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
